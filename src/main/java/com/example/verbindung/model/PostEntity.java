f34a14e918b4590ef39ed86874ee0ccb1d41475c
Florian Ruffner <fr@ruffner-beratung.de>
2024-03-28 16:14:07 +0100

    set up

---
+++ src/main/java/com/example/verbindung/model/PostEntity.java
@@ -0,0 +1,9 @@
+package com.example.verbindung.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "NAME", schema = "falls vohanden") //ändern auf unser werte
+public class PostEntity {
+
+}
