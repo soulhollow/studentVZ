bcf81280d1cbbaab21dfee299e9c18196d140d29
Florian Ruffner <fr@ruffner-beratung.de>
2024-03-30 14:42:30 +0100

    funktioniert noch nicht ganz Setup für Frontend

---
+++ src/test/java/com/example/verbindung/VerbindungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.verbindung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbindungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
