89f6759301a28e7da09b99825b46c494f130cd11
Florian <fr@ruffner-beratung.de>
2024-04-22 17:13:11 +0200

    test

--- Frontend/studentvz/src/app/main-feed/main-feed.service.ts
+++ Frontend/studentvz/src/app/main-feed/main-feed.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainFeedService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+  reload(){
+    console.log("Main Feed Service");
+    
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    this.http.get<Message[]>('http://localhost:8080/api/messages/all', { headers: headers })
+      .subscribe(response => {
+        console.log(response);
+        // Process the response here
+        // Assuming Message is the interface representing the message structure
+        // You can replace it with your actual interface or type
+        response.forEach((message: Message) => {
+          console.log('ID:', message.id);
+          console.log('Header:', message.header);
+          console.log('Content:', message.content);
+          console.log('Timestamp:', message.timestamp);
+          console.log('user_ID:', message.userId);
+        });
+      }, error => {
+        console.error(error);
+      });
+  }
 }
