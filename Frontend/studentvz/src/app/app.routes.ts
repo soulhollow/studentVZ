e44721c692cec84c7363bbde34a62dae5fb8b292
Florian Ruffner <fr@ruffner-beratung.de>
2024-04-24 19:43:40 +0200

    mal schauen

--- Frontend/studentvz/src/app/app.routes.ts
+++ Frontend/studentvz/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import {CreatePostComponent} from "./create-post/create-post.component";
+import { MainFeedComponent } from './main-feed/main-feed.component';
 
 export const routes: Routes = [ { path: 'login', component: LoginComponent },
   { path: 'create', component: CreatePostComponent },
